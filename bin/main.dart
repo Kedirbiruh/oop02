70437ed407c45277ef03f831d4483ab543207c46
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-09-26 15:17:48 +0200

    calculate aree

--- bin/main.dart
+++ bin/main.dart
@@ -1,20 +1,18 @@
 import '../lib/triangle.dart';
+
 void main() {
-  final triangleCm = Triangle.cm(30, 40);
-  print(triangleCm); 
+  final t1 = Triangle.mm(100, 40);
+  final t2 = Triangle.cm(40, 20);
+  final t3 = Triangle.dm(10, 4);
+  final t4 = Triangle.m(7, 9);
+  final t5 = Triangle.from(2.5, 3.0, MeasurementSystem.m);
 
-  print("Fläche in cm²: ${triangleCm.areaInMm2}");
+  print(t1);
+  print(t2);
+  print(t3);
+  print(t4);
+  print(t5);
 
-final triangleDm = Triangle.cm(30, 40);
-  print(triangleDm); 
-
-  print("Fläche in cm²: ${triangleDm.areaInMm2}");
-
-
-
-
+  print("Fläche in cm²: ${t1.areaInMm2}");
+  print("Fläche in m²: ${t4.areaInMm2}");
 }
-
-
-
-
